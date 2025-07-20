1115e79779f2213c117e82ed3a8df1c17ece3c53
Hyteq <DerpsDer.es@gmail.com>
2025-07-20 18:03:05 +0300

    more cleaning

--- apps/docs/app/(home)/layout.tsx
+++ apps/docs/app/(home)/layout.tsx
@@ -1,6 +1,12 @@
 import type { ReactNode } from 'react';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/app/layout.config';
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+const baseOptions: BaseLayoutProps = {
+  nav: {
+    enabled: false,
+  },
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <HomeLayout {...baseOptions}>
