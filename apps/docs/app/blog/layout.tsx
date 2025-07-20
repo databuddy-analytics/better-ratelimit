7e1aeba419024ed6838b72e4d39af383a487f113
Hyteq <DerpsDer.es@gmail.com>
2025-07-20 18:05:00 +0300

    more cleaning

--- apps/docs/app/blog/layout.tsx
+++ apps/docs/app/blog/layout.tsx
@@ -1,12 +1,18 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
 import { blogSource } from '@/lib/blog-source';
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+const baseOptions: BaseLayoutProps = {
+  nav: {
+    enabled: false,
+  },
+}
 
 export default function BlogLayout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout 
-      tree={blogSource.pageTree} 
+    <DocsLayout
+      tree={blogSource.pageTree}
       {...baseOptions}
     >
       {children}
--- apps/docs/app/docs/layout.tsx
+++ apps/docs/app/docs/layout.tsx
@@ -1,7 +1,14 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+const baseOptions: BaseLayoutProps = {
+  nav: {
+    enabled: false,
+  },
+}
+
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
